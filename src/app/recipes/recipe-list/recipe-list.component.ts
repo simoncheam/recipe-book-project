17358b4a2b333ad584bb249d6c50179092d615ab
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-18 21:12:30 -0400

    debugging section 6 - components and databinding, recipe detail not displaying when clicking recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 
@@ -9,12 +9,21 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a Test Recipe', 'https://getsmartratsolutions.com/wp-content/uploads/2021/11/thanksgiving-recipe-ideas.jpg'),
     new Recipe('A Test Recipe', 'This is a Test Recipe', 'https://getsmartratsolutions.com/wp-content/uploads/2021/11/thanksgiving-recipe-ideas.jpg'),
   ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
+
   }
 
 }
