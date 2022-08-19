08a47dd4e04c1cd52f86b9f7d57139c10813ebf6
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-19 11:47:48 -0400

    add item to shopping list is working

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -20,4 +20,9 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+
+    this.ingredients.push(ingredient);
+  }
+
 }
